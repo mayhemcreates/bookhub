b25959fa53662046f88ce794936a10c849c1af36
harrie mayhew <hm@initials.co.uk>
2026-02-21 20:29:18 +0000

    icon update (as normal component0

--- BookHub.Web/src/components/BooksToolbar.vue
+++ BookHub.Web/src/components/BooksToolbar.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import IconGrid from '@/assets/icons/IconGrid.vue'
 import IconList from '@/assets/icons/IconList.vue'
-
+import IconSearch from '@/assets/icons/IconSearch.vue'
 // TODO implement search
 const updateSearch = (event: Event) => {
   const target = event.target as HTMLInputElement
@@ -18,12 +18,7 @@ const updateSearch = (event: Event) => {
       <label for="search" class="sr-only">
         <span class="sr-only">Search</span>
         <span class="search__placeholder">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
-            <path
-              d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z"
-              fill="var(--color-grey-2)"
-            />
-          </svg>
+          <IconSearch />
           <span>Search by title or author</span>
         </span>
         <input type="text" id="search" @input="updateSearch" />
@@ -75,14 +70,16 @@ const updateSearch = (event: Event) => {
     gap: 8px;
 
     span {
-      color: var(--color-grey-2);
+      color: var(--color-grey-3);
     }
 
     svg {
       width: 25px;
       height: 25px;
       margin-right: 8px;
+      stroke: var(--color-grey-3);
     }
+
   }
 
   label {
@@ -98,7 +95,7 @@ const updateSearch = (event: Event) => {
   display: flex;
   border-radius: 10px;
   background-color: var(--color-white);
-  border: 1px solid var(--color-grey-2);
+  border: 1px solid var(--color-grey-3);
   height: 48px;
 
   &__option {
