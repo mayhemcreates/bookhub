37eaa0de9cd4bfaf5934cbcdf6904185d21cbc36
harrie mayhew <hm@initials.co.uk>
2026-02-21 18:37:15 +0000

    Add vite svg loader for performant svg rendering, implement svgs as vue components, add nav icons to the route objects to be rendered in sidebar

--- BookHub.Web/src/router/index.ts
+++ BookHub.Web/src/router/index.ts
@@ -1,12 +1,21 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createMemoryHistory, createRouter, type RouteRecordRaw } from 'vue-router'
 
 import MyBooks from '@/views/MyBooks.vue'
+import IconAnalytics from '@/assets/icons/IconAnalytics.vue'
+import IconBooks from '@/assets/icons/IconBooks.vue'
+import IconDashboard from '@/assets/icons/IconDashboard.vue'
+import IconSettings from '@/assets/icons/IconSettings.vue'
+import type { Component } from 'vue/dist/vue.js'
 
-const routes = [
-  { path: '/', name: 'Dashboard', component: MyBooks },
-  { path: '/books', name: 'My Books', component: MyBooks },
-  { path: '/analytics', name: 'Analytics', component: MyBooks },
-  { path: '/settings', name: 'Settings', component: MyBooks },
+export type CustomRouteRecordRaw = RouteRecordRaw & {
+  icon?: Component
+}
+
+const routes: CustomRouteRecordRaw[] = [
+  { path: '/', name: 'Dashboard', component: MyBooks, icon: IconDashboard },
+  { path: '/books', name: 'My Books', component: MyBooks, icon: IconBooks },
+  { path: '/analytics', name: 'Analytics', component: MyBooks, icon: IconAnalytics },
+  { path: '/settings', name: 'Settings', component: MyBooks, icon: IconSettings },
 ]
 
 export const router = createRouter({
